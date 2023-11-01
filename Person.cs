2356bde42c8bd57173cd463e7a5004356af8d23c
gabriella <gabri@DESKTOP-U2VKQFP>
2023-11-01 14:24:12 +0100

    Lade till rvå klasser som ärver av basklassen

---
+++ Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegistryGroupExercise
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public int PersonalNumber { get; set; }
+
+        public Person(string firstname, string lastname, string role, int personalnumber)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Role = role;
+            PersonalNumber = personalnumber;
+        }
+
+        public virtual void PrintInfo()
+        {
+
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegistryGroupExercise
+{
+    internal class Teacher : Person
+    {
+        public string Subject { get; set; }
+
+        public Teacher(string subject, string firstname, string lastname, string role, int personalnumber)
+            : base (firstname, lastname, role, personalnumber)
+        {
+            Subject = subject;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Förnamn: {FirstName} Efternamn: {LastName}  Roll: {Role} Undervisningsämne: {Subject} );
+        }
+
+    }
+}
