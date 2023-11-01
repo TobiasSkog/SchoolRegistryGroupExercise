09a95e4359bd2b3a3e7dcddec5ad83cdc2c936e8
Ziano <Ziano@ZIA>
2023-11-01 14:20:27 +0100

    hej

---
+++ Logic/Return.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegistryGroupExercise.Logic
+{
+    internal class Return
+    {
+        public void PrintRoleInfo(string role)
+        {
+            Console.WriteLine($"Förnamn: {FirstName}, Efternamn: {LastName}, Roll: {Role}, Personnummer: {PersonalNumber}");
+            switch (role)
+            {
+                case "Lärare":
+                    Console.WriteLine("Detta är en lärare. Ämne: " + Role);
+                    break;
+                case "Student":
+                    Console.WriteLine("Detta är en student. Kurs: " + Role);
+                    break;                   
+            }
+        }
+    }
+}
