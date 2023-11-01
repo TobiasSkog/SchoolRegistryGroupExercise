42ddf8c16c19a2269ba695dc2f2dcd379dfed717
Emma <elune.lind@gmail.com>
2023-11-01 16:55:53 +0100

    returvärde bool i Menu()

--- Meny/Meny.cs
+++ Meny/Meny.cs
@@ -2,10 +2,10 @@
 {
     public static class Meny
     {
-        public static void Menu()
+        public static bool Menu()
         {
             string menuchoice = "";
-
+            
             while (menuchoice != "0" || menuchoice != "exit")
             {
                 PrintMenu();
@@ -34,24 +34,25 @@
                         Console.ForegroundColor = ConsoleColor.Green;
                         Console.WriteLine("Tack för idag!");
                         Console.ForegroundColor = ConsoleColor.White;
-                        break;
+                        return false;
                     default:
                         Console.WriteLine("Välj någon av kategorierna ovan.\n");
                         break;
                 }
             }
+            return true;
         }
         public static void PrintMenu()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("***** Skolregistret *****");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("1. Lägg till lärare");
-            Console.WriteLine("2. Lägg till elev");
-            Console.WriteLine("3. Skriv ut alla lärare");
-            Console.WriteLine("4. Skriv ut alla studerande");
-            Console.WriteLine("5. Skriv ut alla i registret");
-            Console.WriteLine("\n 0. Avsluta\n");
+            Console.WriteLine("(1) Lägg till lärare");
+            Console.WriteLine("(2) Lägg till elev");
+            Console.WriteLine("(3) Skriv ut alla lärare");
+            Console.WriteLine("(4) Skriv ut alla studerande");
+            Console.WriteLine("(5) Skriv ut alla i registret");
+            Console.WriteLine("\n (0/exit) Avsluta\n");
         }
     }
 }
