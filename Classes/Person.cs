5f76e69ab836d8526b193a06a05b718ab1123eb7
gabriella <gabri@DESKTOP-U2VKQFP>
2023-11-01 14:32:59 +0100

    Lade til några saker, bland annat ID och abstract class

--- Classes/Person.cs
+++ Classes/Person.cs
@@ -1,7 +1,29 @@
-﻿namespace SchoolRegistryGroupExercise.Classes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegistryGroupExercise.Classes
 {
     public abstract class Person
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public int PersonalNumber { get; set; }
 
+        public Person(string firstname, string lastname, string role, int personalnumber)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Role = role;
+            PersonalNumber = personalnumber;
+        }
+
+        public virtual void PrintInfo()
+        {
+
+        }
     }
 }
---
+++ Classes/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolRegistryGroupExercise.Classes
+{
+    internal class Student : Person
+    {
+        public string Course { get; set; }
+
+        public Student(string course, string firstname, string lastname, string role, int personalnumber)
+            : base(firstname, lastname, role, personalnumber)
+        {
+            Course = course;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"Förnamn: {FirstName} Efternamn: {LastName}  Roll: {Role} Kurs: {Course} Id: {PersonalNumber}");
+        }
+    }
+}
