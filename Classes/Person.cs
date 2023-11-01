9acc64277ee9c78a38fa118db46ca5fbaa714c5e
Tobias Skog <t_ott_e@hotmail.com>
2023-11-01 13:44:31 +0100

    Add project files.

---
+++ Classes/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace SchoolRegistryGroupExercise.Classes
+{
+    public abstract class Person
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace SchoolRegistryGroupExercise
+{
+
+    public enum Role
+    {
+        Teacher,
+        Student
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> minLista = new();
+
+            //FirstName, LastName, Role och PersonalNumber
+            //switch beroende på lärare / studnet
+            // Lärare = subjekt (string)
+            // Student = Course (string)
+
+
+        }
+    }
+}
