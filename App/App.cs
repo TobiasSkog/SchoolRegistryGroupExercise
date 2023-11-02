6b2df930ae7174b32658496e14a7a2e276ff08aa
Tobias Skog <t_ott_e@hotmail.com>
2023-11-02 11:54:24 +0100

    ye

--- App/App.cs
+++ App/App.cs
@@ -1,6 +1,4 @@
 ﻿using SchoolRegistryGroupExercise.Classes;
-using SchoolRegistryGroupExercise.ConsoleOut;
-using SchoolRegistryGroupExercise.Logic;
 
 namespace SchoolRegistryGroupExercise
 {
@@ -19,89 +17,10 @@ namespace SchoolRegistryGroupExercise
         private void Quit()
         {
             RunApp = false;
+
         }
-        public void Run()
-        {
-            while (RunApp)
-            {
-                Console.Clear();
-                if (Register.Count == 0)
-                {
-                    // Print Add To List Meny
-                    Meny.PrintMenuEmptyRegister();
-
-                    string menuChoice = Console.ReadLine();
-
-                    switch (menuChoice)
-                    {
-                        case "1":
-                            //lägg till lärare
-                            var teacher = Return.CreateClassBaseOnUserInput(Role.Teacher);
-                            Register.Add(teacher);
-
-                            break;
-                        case "2":
-                            //lägg till elev
-                            var student = Return.CreateClassBaseOnUserInput(Role.Student);
-                            Register.Add(student);
-                            break;
-                        case "0":
-                        case "exit":
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine("Tack för idag!");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Quit();
-                            break;
-                        default:
-                            Console.WriteLine("Välj någon av kategorierna ovan.\n");
-                            break;
-                    }
-                }
-                else
-                {
-                    Meny.PrintFullMenu();
-
-                    string menuChoice = Console.ReadLine();
-
-                    switch (menuChoice)
-                    {
-                        case "1":
-                            //lägg till lärare
-                            var teacher = Return.CreateClassBaseOnUserInput(Role.Teacher);
-                            Register.Add(teacher);
-
-                            break;
-                        case "2":
-                            //lägg till elev
-                            var student = Return.CreateClassBaseOnUserInput(Role.Student);
-                            Register.Add(student);
-                            break;
-                        case "3":
-                            //skriver ut alla lärare
-                            PrintAllTeachers.PrintTeachers(Register);
-                            break;
-                        case "4":
-                            //skriv ut alla studerande
-                            PrintAllStudents.PrintStudents(Register);
-                            break;
-                        case "5":
-                            //skriv ut alla
-                            PrintEverything.PrintEveryone(Register);
-                            break;
-                        case "0":
-                        case "exit":
-                            Console.ForegroundColor = ConsoleColor.Green;
-                            Console.WriteLine("Tack för idag!");
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Quit();
-                            break;
-                        default:
-                            Console.WriteLine("Välj någon av kategorierna ovan.\n");
-                            break;
-                    }
-                }
-            }
-        }
+    }
+}
     }
 }
 
